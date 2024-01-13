1ad13f7a5f161a767ffd895ba1dab921258f5815
Herman <43386816+DrGermanius@users.noreply.github.com>
2024-01-13 22:26:50 +0100

    haters gonna hate

--- fanin.go
+++ fanin.go
@@ -9,23 +9,28 @@ type hchan struct {
 	dataqsiz uint
 	buf      unsafe.Pointer
 	elemsize uint16
+	elemtype uint
+	sendx    uint   
 }
 
 //go:noinline
 func In[T any](ch ...*chan T) chan T {
 	var buf []T
 	var qcount uint
+	fan := make(chan T, 1)
 	for i := range ch {
 		p := (*hchan)(unsafe.Pointer((*(*uintptr)(unsafe.Pointer(ch[i])))))
 		qcount += p.qcount
 
+		*ch[i] = fan
+
 		s := unsafe.Slice((*T)(p.buf), p.qcount)
 		buf = append(buf, s...)
 	}
-	fan := make(chan T, qcount)
 	p := (*hchan)(unsafe.Pointer((*(*uintptr)(unsafe.Pointer(&fan)))))
-	p.qcount = qcount
+	p.qcount = qcount 
+	p.dataqsiz = qcount + 1 
 	p.buf = unsafe.Pointer(unsafe.SliceData(buf))
-
+	p.sendx = qcount
 	return fan
 }
