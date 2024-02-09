6e6c4f0391efc7eea738847de2aaf1dd5292ed33
Herman <43386816+DrGermanius@users.noreply.github.com>
2024-02-09 17:15:01 +0100

    Update fanin_test.go

--- fanin_test.go
+++ fanin_test.go
@@ -3,6 +3,7 @@ package fan
 import (
 	"fmt"
 	"reflect"
+	"sync"
 	"testing"
 )
 
@@ -32,7 +33,7 @@ func TestIn(t *testing.T) {
 		t.Fatal("Source code is lying")
 	}
 }
-func TestInCanDoSendToOldChan(t *testing.T) {
+func TestIn_CanDoSendToOldChan(t *testing.T) {
 	ch1 := make(chan int, 1)
 	ch1 <- 11
 	ch2 := make(chan int, 2)
@@ -43,6 +44,14 @@ func TestInCanDoSendToOldChan(t *testing.T) {
 	ch3 <- 32
 	res := In[int](&ch1, &ch2, &ch3)
 	max := len(res)
+
+	wg := sync.WaitGroup{}
+	wg.Add(1)
+	go func(){
+		<-res
+		wg.Done()
+	}()
+	wg.Wait()
 
 	ch3 <- 33 // can do
 
@@ -56,13 +65,52 @@ func TestInCanDoSendToOldChan(t *testing.T) {
 		}
 	}
 
-	if !reflect.DeepEqual(results, []int{11, 21, 22, 31, 32, 33}) {
+	if !reflect.DeepEqual(results, []int{21, 22, 31, 32, 33}) {
 		fmt.Println(results)
 		t.Fatal("Source code is lying")
 	}
 }
 
-func TestIn_empty(t *testing.T) {
+func TestIn_GeiGin(t *testing.T){
+	ch1 := make(chan int, 1)
+	ch2 := make(chan int, 1)
+	wg := sync.WaitGroup{}
+	wg.Add(3)
+
+	cc := In[int](&ch1, &ch2)
+	var results []int
+	go func() {
+		i := 0
+		for v := range cc {
+			results = append(results, v)
+			i++
+			if i == 2 {
+				break
+			}
+		}
+		wg.Done()
+	}()
+
+	go func() {
+		ch1 <- 1
+		//close(ch1)
+		wg.Done()
+	}()
+
+	go func() {
+		ch2 <- 2
+		//close(ch2)
+		wg.Done()
+	}()
+	wg.Wait()
+
+	if !(reflect.DeepEqual(results, []int{1 ,2}) || reflect.DeepEqual(results, []int{2, 1})) {
+		fmt.Println(results)
+		t.Fatal("Source code is lying")
+	}
+}
+
+func TestIn_Empty(t *testing.T) {
 	ch1 := make(chan int, 1)
 	ch2 := make(chan int, 2)
 	ch3 := make(chan int, 3)
